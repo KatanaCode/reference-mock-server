4ff48db05213d45a505173414a25f58cf8625b49
daniel <daniel.kuras@openbanking.org.uk>
2017-11-10 13:59:52 +0000

    Removed unused dependencies

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 if (!process.env.DEBUG) process.env.DEBUG = 'error,log';
 
-const port = (process.env.PORT || 8001);
-const log = require('debug')('log');
+const securedPort = (process.env.SECURED_PORT || 4433);
+const https = require('https');
+
 const { initApp } = require('./lib');
 
+const serverCert = Buffer.from(process.env.SERVER_CERT || '', 'base64').toString();
+const serverKey = Buffer.from(process.env.SERVER_KEY || '', 'base64').toString();
+const caCert = Buffer.from(process.env.CA_CERT || '', 'base64').toString();
+
+const options = {
+  key: serverKey,
+  cert: serverCert,
+  ca: caCert,
+  requestCert: true,
+  rejectUnauthorized: true,
+};
+
 initApp((app) => {
-  app.listen(port, () => {
-    log(`running on localhost:${port} ... `);
-  });
+  https.createServer(options, app).listen(securedPort);
 });
